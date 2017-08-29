f0158db7a76ef44f628e9bdc3d32cb04da9e2c6c
dennyzhang <denny@dennyzhang.com>
2017-08-28 20:38:38 -0500

    get parameters from env

--- transfer_folder_by_http.sh
+++ transfer_folder_by_http.sh
@@ -10,14 +10,17 @@
 ## Read more: https://www.dennyzhang.com/transfer_cli
 ## --
 ## Created : <2017-08-28>
-## Updated: Time-stamp: <2017-08-28 20:35:00>
+## Updated: Time-stamp: <2017-08-28 20:38:28>
 ##-------------------------------------------------------------------
-folder_full_path=${1?}
-max_download=${2:-"1"}
-max_days=${3:-"1"}
+if [ -z "$FOLDER_FULL_PATH" ]; then
+    echo "ERROR: Need to provide FOLDER_FULL_PATH"
+fi
 
-parent_folder_name="$(dirname "$folder_full_path")"
-folder_name="$(basename "$folder_full_path")"
+[ -n "$MAX_DAYS" ] || MAX_DAYS="1"
+[ -n "$MAX_DOWNLOAD" ] || MAX_DOWNLOAD="1"
+
+parent_folder_name="$(dirname "$FOLDER_FULL_PATH")"
+folder_name="$(basename "$FOLDER_FULL_PATH")"
 tar_file_name="/tmp/${folder_name}.tar.gz"
 
 cd "$parent_folder_name"
@@ -25,7 +28,7 @@ echo "tar -zcvf $tar_file_name $folder_name"
 tar -zcvf "$tar_file_name" "$folder_name"
 
 echo "Upload $tar_file_name"
-curl -H "Max-Downloads: $max_download" -H "Max-Days: $max_days" \
+curl -H "Max-Downloads: $MAX_DOWNLOAD" -H "Max-Days: $MAX_DAYS" \
      --upload-file "$tar_file_name" "https://transfer.sh/${folder_name}.tar.gz"
 
 echo ""
