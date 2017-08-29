eb71dfe9826202f48a98c204dbc390a8fc8f1842
dennyzhang <denny@dennyzhang.com>
2017-08-28 20:42:35 -0500

    improve log message

--- transfer_file_by_http.sh
+++ transfer_file_by_http.sh
@@ -10,7 +10,7 @@
 ## Read more: https://www.dennyzhang.com/transfer_cli
 ## --
 ## Created : <2017-08-28>
-## Updated: Time-stamp: <2017-08-28 20:40:35>
+## Updated: Time-stamp: <2017-08-28 20:42:29>
 ##-------------------------------------------------------------------
 if [ -z "$TRANSFER_FULL_PATH" ]; then
     echo "ERROR: Need to provide TRANSFER_FULL_PATH"
@@ -25,10 +25,11 @@ folder_name="$(basename "$TRANSFER_FULL_PATH")"
 tar_file_name="/tmp/${folder_name}.tar.gz"
 
 cd "$parent_folder_name"
-echo "tar -zcvf $tar_file_name $folder_name"
-tar -zcvf "$tar_file_name" "$folder_name"
+echo "tar -zcf $tar_file_name $folder_name"
+tar -zcf "$tar_file_name" "$folder_name"
 
-echo "Upload $tar_file_name"
+echo "Upload $tar_file_name."
+echo "The file can be downloaded only $MAX_DOWNLOAD times. And Valid for $MAX_DAYS days"
 curl -H "Max-Downloads: $MAX_DOWNLOAD" -H "Max-Days: $MAX_DAYS" \
      --upload-file "$tar_file_name" "https://transfer.sh/${folder_name}.tar.gz"
 
