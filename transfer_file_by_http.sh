f5b983c11e761899f8ea88842ec34f94218e4151
dennyzhang <denny@dennyzhang.com>
2017-08-28 20:40:48 -0500

    rename file

--- transfer_file_by_http.sh
+++ transfer_file_by_http.sh
@@ -4,23 +4,24 @@
 ## Licensed under MIT 
 ##   https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
 ##
-## File : transfer_folder_by_http.sh
+## File : transfer_file_by_http.sh
 ## Author : Denny <denny@dennyzhang.com>
 ## Description :
 ## Read more: https://www.dennyzhang.com/transfer_cli
 ## --
 ## Created : <2017-08-28>
-## Updated: Time-stamp: <2017-08-28 20:38:28>
+## Updated: Time-stamp: <2017-08-28 20:40:35>
 ##-------------------------------------------------------------------
-if [ -z "$FOLDER_FULL_PATH" ]; then
-    echo "ERROR: Need to provide FOLDER_FULL_PATH"
+if [ -z "$TRANSFER_FULL_PATH" ]; then
+    echo "ERROR: Need to provide TRANSFER_FULL_PATH"
 fi
 
 [ -n "$MAX_DAYS" ] || MAX_DAYS="1"
 [ -n "$MAX_DOWNLOAD" ] || MAX_DOWNLOAD="1"
 
-parent_folder_name="$(dirname "$FOLDER_FULL_PATH")"
-folder_name="$(basename "$FOLDER_FULL_PATH")"
+# TODO: support both file and folder
+parent_folder_name="$(dirname "$TRANSFER_FULL_PATH")"
+folder_name="$(basename "$TRANSFER_FULL_PATH")"
 tar_file_name="/tmp/${folder_name}.tar.gz"
 
 cd "$parent_folder_name"
@@ -32,4 +33,4 @@ curl -H "Max-Downloads: $MAX_DOWNLOAD" -H "Max-Days: $MAX_DAYS" \
      --upload-file "$tar_file_name" "https://transfer.sh/${folder_name}.tar.gz"
 
 echo ""
-## File : transfer_folder_by_http.sh ends
+## File : transfer_file_by_http.sh ends
